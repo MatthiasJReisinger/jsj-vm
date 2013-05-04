c53b6b36bcd469516859cf424a0eabfc4e415fc0
d412vv1n <mr@master-studios.net>
2013-05-04 17:08:53 +0200

    -

---
+++ src/ParsedClassFile.js
@@ -0,0 +1,18 @@
+function ParsedClassFile() {
+    var magic;
+    var minor_version;
+    var major_version;
+    var constant_pool_count;
+    var constant_pool;
+    var access_flags;
+    var this_class;
+    var super_class;
+    var interfaces_count;
+    var interfaces;
+    var fields_count;
+    var fields;
+    var methods_count;
+    var methods;
+    var attributes_count;
+    var attributes;
+}
